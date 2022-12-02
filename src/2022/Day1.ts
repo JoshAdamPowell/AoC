18f51dee727b512b147a5bc81ec70ffa68f3c879
Josh Powell <josh.powell@softwire.com>
2022-12-02 11:36:07 +0000

    refactor day 1

--- src/2022/Day1.ts
+++ src/2022/Day1.ts
@@ -4,20 +4,16 @@ import Day from "../templates/Day";
 export default class Day1 implements Day{
     day = 1
     getPartOne = (): number => {
-        const input = FileReader.getNumericData(this.day.toLocaleString())
-        const elves = input.reduce((acc, cur) => {
-            if (!isNaN(cur)) {
-                acc[acc.length - 1] += cur
-            } else {
-                acc.push(0)
-            }
-            return acc;
-        }, [0])
-        elves.sort((a, b) => a - b)
+        const elves = this.getSortedElves()
         return elves[elves.length - 1]
     }
 
     getPartTwo = (): number => {
+        const elves = this.getSortedElves()
+        return elves[elves.length - 1] + elves[elves.length - 2] + elves[elves.length - 3]
+    }
+
+    getSortedElves = (): number[] => {
         const input = FileReader.getNumericData(this.day.toLocaleString())
         const elves = input.reduce((acc, cur) => {
             if (!isNaN(cur)) {
@@ -28,6 +24,6 @@ export default class Day1 implements Day{
             return acc;
         }, [0])
         elves.sort((a, b) => a - b)
-        return elves[elves.length - 1] + elves[elves.length - 2] + elves[elves.length - 3]
+        return elves;
     }
 }
