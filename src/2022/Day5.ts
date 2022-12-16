37b3bc16428160e49766c6be5c7241cdd7518f2b
Josh Powell <josh.powell@softwire.com>
2022-12-16 15:11:05 +0000

    day 5

--- src/2022/Day5.ts
+++ src/2022/Day5.ts
@@ -3,11 +3,47 @@ import Day from "../templates/Day";
 
 export default class Day5 implements Day {
     day = 5
-    getPartOne = (): number => {
-        throw new Error("Method not implemented.");
+    // I want the stacks array to be one-indexed so theres an empty array first
+    stacks = [[], Array.from("WTHPJCF").reverse(),
+    Array.from("HBJZFVRG").reverse(),
+    Array.from("RTPH").reverse(),
+    Array.from("THPNSZ").reverse(),
+    Array.from("DCJHZFVN").reverse(),
+    Array.from("ZDWFGMP").reverse(),
+    Array.from("PDJSWZVM").reverse(),
+    Array.from("SDN").reverse(),
+    Array.from("MFSZD").reverse()]
+
+    getPartOne = (): string => {
+    //    const regex =  /move (\d+) from (\d+) to (\d+)/
+    //    const input = FileReader.getStringData(this.day.toLocaleString())
+    //    input.forEach(x => {
+    //     const [_, amount, from, to] = Array.from(x.match(regex)!, x => parseInt(x))
+    //     for (let i =0; i < amount; i++) {
+    //         console.log(this.stacks[from])
+    //         const moving = this.stacks[from].pop()
+    //         this.stacks[to].push(moving!)
+    //     }
+    //    })
+    //    console.log(this.stacks)
+       throw "fuck off"
     }
 
     getPartTwo = (): number => {
-        throw new Error("Method not implemented.");
+        const regex =  /move (\d+) from (\d+) to (\d+)/
+        const input = FileReader.getStringData(this.day.toLocaleString())
+        input.forEach(x => {
+         const [_, amount, from, to] = Array.from(x.match(regex)!, x => parseInt(x))
+         const moving = this.stacks[from].splice(this.stacks[from].length - amount, amount)
+         this.stacks[to] = this.stacks[to].concat(moving)
+
+        //  for (let i =0; i < amount; i++) {
+        //      console.log(this.stacks[from])
+        //      const moving = this.stacks[from].pop()
+        //      this.stacks[to].push(moving!)
+        //  }
+        })
+        console.log(this.stacks)
+        throw "fuck off"
     }
 }
