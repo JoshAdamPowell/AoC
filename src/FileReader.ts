4b52806ec2597686a06d022f3d8fd9bf0db14304
Josh Powell <josh.powell@softwire.com>
2023-12-24 17:04:36 +0000

    commonise years

--- src/FileReader.ts
+++ src/FileReader.ts
@@ -1,9 +1,13 @@
 import fs from "fs";
+import dotenv from 'dotenv'
+dotenv.config()
 
 export default class FileReader {
 
     static getStringData = (fileName: string): string[] => {
-        const path = `src/data/${fileName}.txt`;
+        dotenv.config()
+
+        const path = `src/data/${process.env.YEAR}${fileName}.txt`;
         const file = fs.readFileSync(path, "utf-8");
         const regex = /\n/
         return file.split(regex)
